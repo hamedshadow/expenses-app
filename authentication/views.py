31c0beafc1b1e3b85e72b1fa9be7f60887907526
ubuntu <hamed.jafarian.dev@gamil.com>
2024-01-13 13:26:54 +0000

    ch(deploy): register form creation

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 
+
+from expenseswebsite import settings
 # Create your views here.
 
 
 class RegisterationView(View):
     def get(self, request):
+        # args = {}
+        # args['my_title'] = settings.VARIABLES[1]
+        # args need too pass in below  render command ,args
         return render(request, 'authentication/register.html')
--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# VARIABLES = ["Sag", "Gorbe"]
 STATIC_URL = 'static/'
 STATIC_DIR = [os.path.join(BASE_DIR,'expenseswebsite/static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
