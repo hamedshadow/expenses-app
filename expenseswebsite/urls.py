31c9cbc47292471c4cf3c81044c0e23e6177e971
ubuntu <hamed.jafarian.dev@gamil.com>
2024-03-05 10:45:20 +0000

    dropdownlist1

--- expenseswebsite/urls.py
+++ expenseswebsite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', include('expenses.urls')),
     path('authentication/', include('authentication.urls')),
     path('admin/', admin.site.urls),
+    path('preferences/', include('userpreferences.urls')),
 ]
--- userpreferences/views.py
+++ userpreferences/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import os
+import json 
+from django.conf import settings
 
 # Create your views here.
+
+
+def index(request):
+    currency_data = []
+    file_path= os.path.join(settings.BASE_DIR, 'currencies.json') 
+    import pdb
+    pdb.set_trace()
+    return render(request,'preferences/index.html')
