3650c95be92152e3ffe31507158fe0527c2eb776
ubuntu <hamed.jafarian.dev@gamil.com>
2024-03-05 10:46:29 +0000

    dropdownlist2

--- userpreferences/views.py
+++ userpreferences/views.py
@@ -9,6 +9,9 @@ from django.conf import settings
 def index(request):
     currency_data = []
     file_path= os.path.join(settings.BASE_DIR, 'currencies.json') 
-    import pdb
-    pdb.set_trace()
+    with open(file_path,'r') as json_file:
+        
+    
+        import pdb
+        pdb.set_trace()
     return render(request,'preferences/index.html')
